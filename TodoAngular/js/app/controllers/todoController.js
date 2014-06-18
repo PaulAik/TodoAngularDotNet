30c2fccdd58afe49e7c53d128caad77cb6a7db2a
Paul Aikman <paulaik@gmail.com>
2014-06-18 12:48:54 +0100

    - Tidy up.

--- TodoAngular/js/app/controllers/todoController.js
+++ TodoAngular/js/app/controllers/todoController.js
@@ -1,6 +1,6 @@
-﻿app.controller('todoCtrl', function ($scope, todoService) {
+﻿app.controller('todoCtrl', function($scope, todoService) {
 
-	todoService.getTodos().success(function (todos) {
+	todoService.getTodos().success(function(todos) {
 		$scope.todos = todos;
 	});
 
@@ -8,13 +8,16 @@
 
 	//$scope.newTodo = { Name: "", Done: false };
 
-	$scope.addTodo = function () {
+	$scope.addTodo = function() {
 		$scope.submitted = true;
 
 		if ($scope.todoForm.$valid) {
 
-			todoService.submitTodo($scope.newTodo).success(function () {
-				$scope.todos.push({ Name: $scope.newTodo.Name, Done: false });
+			todoService.submitTodo($scope.newTodo).success(function() {
+				$scope.todos.push({
+					Name: $scope.newTodo.Name,
+					Done: false
+				});
 				$scope.newTodo = '';
 
 				$scope.submitted = false;
@@ -22,17 +25,17 @@
 		}
 	};
 
-	$scope.updateTodo = function (todo) {
-		todoService.updateTodo(todo).success(function () {
-			
+	$scope.updateTodo = function(todo) {
+		todoService.updateTodo(todo).success(function() {
+
 		});
 	};
 
-	$scope.$watch('todos', function (newValue, oldValue) {
+	$scope.$watch('todos', function(newValue, oldValue) {
 		console.log('test');
 		var count = 0;
 
-		angular.forEach($scope.todos, function (todo) {
+		angular.forEach($scope.todos, function(todo) {
 			count += todo.Done ? 1 : 0;
 		});
 
